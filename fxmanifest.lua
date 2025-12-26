b869a88a7cdea6468a8f20f20e130ac52be14006
Joey <jkhoshaba315@gmail.com>
2025-12-25 22:38:21 -0800

    fxmanifest.lua
    This file defines the fxmanifest for a GTA V resource.

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Joey (Discord: joeycfx)'
+description 'Disables AI ped and vehicle population spawning for routing bucket 0'
+
+server_script 'main.lua'
